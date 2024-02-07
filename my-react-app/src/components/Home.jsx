ff61c74deda87f2f5960267f7d2df60ad86ec01f
Asavela2 <asavelamgiddi00@gmail.com>
2024-02-07 10:57:43 +0200

    Update all

---
+++ my-react-app/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="Home" id="home">
+     <h1>Hello</h1> 
+    </div>
+  );
+}
+
+export default Home;
